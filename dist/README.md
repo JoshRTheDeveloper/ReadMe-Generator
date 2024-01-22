2129fd216973e64c0b1e5dba196796fa04b33208
Josh Roth <you@example.comraider4414@gmail.com>
2024-01-22 16:09:31 -0600

    Completed code - need to test

---
+++ dist/README.md
@@ -0,0 +1,33 @@
+#  
+![Github license](https://img.shields.io/badge/license--blue.svg)
+
+## Description
+
+
+## Installation Instructions
+
+
+## Table of Contents
+* [Usage Information](#usage-information)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Email](#email)
+
+## Usage Information
+
+
+## Contributors
+
+
+## Test.o
+
+
+## Contact
+
+
+## Email
+
+
+## License
+
+This project is licensed under the [ license]([License](https://opensource.org/licenses/)).
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,70 @@
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+
+This project is licensed under the [${license} license](${renderLicenseLink(license)}).`;
+  }
+  return "";
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `# ${data.title} 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Installation Instructions
+${data.installation}
+
+## Table of Contents
+* [Usage Information](#usage-information)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Email](#email)
+
+## Usage Information
+${data.usage}
+
+## Contributors
+${data.contributors}
+
+## Test
+${data.test}
+
+## Contact
+${data.contact}
+
+## Email
+${data.email}
+
+${licenseSection}`;
+}
+
+// Export the generateMarkdown function
+module.exports = generateMarkdown;
